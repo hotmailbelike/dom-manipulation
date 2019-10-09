279544fd552dc405e925e8b84cfb30a31cffffa2
hotmailbelike <fishilike.fish@gmail.com>
2019-10-09 08:51:51 +0600

    Task 2

--- script.js
+++ script.js
@@ -2,8 +2,20 @@
 var heading = document.getElementById('heading');
 console.log(heading.textContent);
 
-/* Task 2 using plain JavaScript */
+/*
+Changing text of heading
+*/
 
+heading.innerHTML = "Fish";
+
+/* Task 2 using plain JavaScript */
+function addText(event){
+    // console.log(event.target); //Telling me which node got clicked
+    var p = document.createElement('p');
+    p.innerText="Hello World";
+    var div = document.getElementById('task2a');
+    div.appendChild(p);
+}
 
 
 /* Task 4 using jQuery */
